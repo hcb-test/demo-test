a692f4b43f5c801f99098903bc308e61545ae9c4
‘hcb’ <404536119@qq.com>
2022-12-18 20:55:38 +0800

    first commit

---
+++ src/main/java/com/example/demotest/controller/DemoTestController.java
@@ -0,0 +1,21 @@
+package com.example.demotest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(value = "/server/v1")
+public class DemoTestController {
+    @RequestMapping(value = "post", method = RequestMethod.POST)
+    public void demoPost(){
+        System.out.println("come in post");
+    }
+
+    @RequestMapping(value = "get", method = RequestMethod.GET)
+    public void demoGet(){
+        System.out.println("come in get");
+    }
+
+}
