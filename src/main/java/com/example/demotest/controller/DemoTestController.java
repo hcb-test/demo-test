8cd785133a819d1094a31838092e376960b4f9ce
“hcb” <1061331412@qq.com>
2022-12-19 20:15:46 +0800

    docker config

--- src/main/java/com/example/demotest/controller/DemoTestController.java
+++ src/main/java/com/example/demotest/controller/DemoTestController.java
@@ -15,6 +15,6 @@ public class DemoTestController {
 
     @RequestMapping(value = "get", method = RequestMethod.GET)
     public String demoGet(){
-        return "come in get -- version 1.0.3";
+        return "devops push deploy success -- version 1.0.6";
     }
 }
